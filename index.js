ea08c9d345a22c94d3139e6dba653aab7e833046
casey.cessnun@inseego.com <casey.cessnun@inseego.com>
2021-02-05 03:55:40 -0800

    new

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ module.exports.handler = async(event)=>{
     app.use(function (req, res, next) {
       res.status(HttpStatus.NOT_IMPLEMENTED).send(JSON.stringify({"error":"Not Implemented"}));
     });
-    //--------------------------
+    //---This Is New---
     app.use(function (req, res, next) {
       res.status(HttpStatus.NOT_FOUND).send(JSON.stringify({"error":"Not Found"}));
     });
