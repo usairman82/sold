9ccdc6553b4069b95640d1ba22bbe7586bb4c846
casey.cessnun@inseego.com <casey.cessnun@inseego.com>
2021-02-05 10:52:22 -0800

    refactoring because of JavaScript jankiness with classes and this reference.

--- modules/Products.js
+++ modules/Products.js
@@ -8,49 +8,49 @@ module.exports.Products = class Products {
     construct(config={}) {
         this.config = config;
         this.utils  = new Utilities(config);
-    }
 
-    async Create(req) {
-        return "Products.Create";
-    }
+        this.Create = async (req)=> {
+            return "Products.Create";
+        };
 
-    async Update(req) {
-        return "Products.Update";
-    }
+        this.Update = async (req)=> {
+            return "Products.Update";
+        };
 
-    //Lots of duplicated code, need iteritive refinement to cleanup
-    //build some helpers.
-    async FetchAll(req) {
-        var response = {"statusCode":HttpStatus.BAD_REQUEST, "error":"Parameter Validation Failed.","details":[]};
-        req = this.utils.ValidateParams(req);
-        if (req.validated)
-        {
-            //Refactor to dedup code
-            console.log(req);
-            delete response.error;
-            response.statusCode = HttpStatus.OK;
-            response.data       = {};
-            var response = await this.db.Query('call fetchPagedProducts',[req.params["page"]*req.params["limit"], req.params["limit"],req.user["userId"]]);
-            if (response.errno || !response) {
-                console.error("MySQL Error", response);
-                return [{
-                            "error": "Database Error - " + response.Error
-                       }];
-                }
+        //Lots of duplicated code, need iteritive refinement to cleanup
+        //build some helpers.
+        this.FetchAll = async (req) =>{
+            var response = {"statusCode":HttpStatus.BAD_REQUEST, "error":"Parameter Validation Failed.","details":[]};
+            req = this.utils.ValidateParams(req);
+            if (req.validated)
+            {
+                //Refactor to dedup code
+                console.log(req);
+                delete response.error;
+                response.statusCode = HttpStatus.OK;
+                response.data       = {};
+                var response = await this.db.Query('call fetchPagedProducts',[req.params["page"]*req.params["limit"], req.params["limit"],req.user["userId"]]);
+                if (response.errno || !response) {
+                    console.error("MySQL Error", response);
+                    return [{
+                                "error": "Database Error - " + response.Error
+                           }];
+                    }
 
-            return response[0];
-        }
-        else
-        {
-            response.details = req.invalidParams;
-        }
-    }
+                return response[0];
+            }
+            else
+            {
+                response.details = req.invalidParams;
+            }
+        };
 
-    async Fetch(req) {
-        return "Products.Fetch";
-    }
+        this.Fetch = async (req)=> {
+            return "Products.Fetch";
+        };
 
-    async Search(req) {
-        return "Products.Search";
+        this.Search = async (req)=> {
+            return "Products.Search";
+        };
     }
 };
